1f825347dca01dcc4d5a4822494960deb17f5741
rushidyadav93-cell <rushidyadav93@gmail.com>
2026-03-30 17:56:13 +0530

    script1.sh

--- script1.sh
+++ script1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Script 1: System Identity Report
-# Author: Prathamesh Kumar | Course: Open Source Software
+# Author: Rushi Yadav | Course: Open Source Software
 
 # --- Variables ---
-STUDENT_NAME="Prathamesh Kumar"
+STUDENT_NAME="Rushi Yadav"
 SOFTWARE_CHOICE="Visual Studio Code"
 
 # --- System Info ---
